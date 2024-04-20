6894cd484991c05c43e3f9f34ca573f370023c7f
Magraina <33145691+magraina@users.noreply.github.com>
2024-04-20 20:06:13 +0200

    Update swagger-initializer.js

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://localhost:3000/api/open-api-spec",
+    url: "https://survey.sheepcs.de/api/open-api-spec",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
